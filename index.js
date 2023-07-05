b8fd0cb338366dcf7b3902fd8011a00fd93b38a9
Anais Weber <anaisweber@MacBook-Pro-de-Anais-2.local>
2023-07-05 13:05:58 +0200

    clean back

--- index.js
+++ index.js
@@ -6,20 +6,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 
 //routes
-const route1 = require("./routes/route1");
-app.use(route1);
-const siret = require("./routes/siret");
-app.use(siret);
 const entreprise = require("./routes/entreprise");
 app.use(entreprise);
-const routest = require("./routes/test");
-app.use(routest);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
